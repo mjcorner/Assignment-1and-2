032ab5fd7dbcbfabadf59c5a160954211d7fee82
mjcorner <mj793591@gmail.com>
2020-02-29 17:36:04 +0530

    Assignment 1 and 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayErrComponent } from './display-err/display-err.component';
+import { EmpListComponent } from './emp-list/emp-list.component';
+import { HomeComponent } from './home/home.component';
+import { EmpDetailsComponent } from './emp-details/emp-details.component';
+import { ResumeComponent } from './resume/resume.component';
+import { GrdFilterPipe } from './grd-filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayErrComponent,
+    EmpListComponent,
+    HomeComponent,
+    EmpDetailsComponent,
+    ResumeComponent,
+    GrdFilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
